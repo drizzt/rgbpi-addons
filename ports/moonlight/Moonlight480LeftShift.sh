d59c6661e884f70fb9986cf3122efdb147b1ee3a
Kevin <99993735+sd2cv@users.noreply.github.com>
2023-10-05 11:33:49 -0700

    Update and rename Moonlight-LeftShift.sh to Moonlight480LeftShift.sh

---
+++ ports/moonlight/Moonlight480LeftShift.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Define the new height and width values
+new_height=480
+new_width=720
+
+# Set the path to the Moonlight.conf file
+moonlight_conf="/root/.config/Moonlight Game Streaming Project/Moonlight.conf"
+
+# Check if the file exists or not
+if [ -f "$moonlight_conf" ]; then
+    # The Moonlight.conf file exists, so update the height and width values
+    sed -i "s/^height=.*/height=${new_height}/" "$moonlight_conf"
+    sed -i "s/^width=.*/width=${new_width}/" "$moonlight_conf"
+    
+    echo "Height and width values in Moonlight.conf have been updated."
+else
+    # The Moonlight.conf file does not exist, so create it with the specified values
+    echo "[General]" > "$moonlight_conf"
+    echo "height=${new_height}" >> "$moonlight_conf"
+    echo "width=${new_width}" >> "$moonlight_conf"
+    
+    echo "Moonlight.conf file created with height and width values."
+fi
+
+echo "Configuration file updated successfully."
+
+# Timings to add, the below 720x480 shifts the image slighly to the left.
+timings=(
+    "320 1 20 32 45 240 1 2 3 16 0 0 0 60.000000 0 6514560 1"
+    "720 1 38 69 108 480 1 3 6 34 0 0 0 30 1 14670150 1"
+    "720 1 29 69 117 576 1 7 6 38 0 0 0 25 1 14656125 1"
+)
+
+file="/opt/rgbpi/ui/data/timings.dat"
+
+# Remove everything except the first line from the file
+sed -i '1,$d' "$file"
+
+# Append the new data to the file
+for line in "${timings[@]}"; do
+    echo "$line" >> "$file"
+done
+
+# Execute moonlight
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+QT_SCALE_FACTOR=0.92 H264_DECODER_HINT=h264_v4l2m2m DRM_FORCE_DIRECT=1 PULSE_SERVER=none QT_QPA_EGLFS_HIDECURSOR=1 QT_QPA_EGLFS_KMS_CONFIG="$SCRIPT_DIR"/moonlight480.json moonlight-qt
