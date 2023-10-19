615b63fd3e50d087be505576bb7e1a606b0bebc8
Kevin <99993735+sd2cv@users.noreply.github.com>
2023-10-19 12:31:04 -0700

    Update install-LXQt-desktop.sh

--- ports/Desktop/install-LXQt-desktop.sh
+++ ports/Desktop/install-LXQt-desktop.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo timedatectl set-timezone UTC
+sudo timedatectl set-ntp true
 sudo apt-get update
 sudo apt-get install lxqt-core xinit xserver-xorg-input-joystick firefox-esr -y
 reboot
