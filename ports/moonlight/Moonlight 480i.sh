c545d88c883882960f5c1ece847063a527fab770
Kevin <99993735+sd2cv@users.noreply.github.com>
2023-10-19 12:32:23 -0700

    Update Moonlight 480i.sh

--- ports/moonlight/Moonlight 480i.sh
+++ ports/moonlight/Moonlight 480i.sh
@@ -25,11 +25,12 @@ fi
 
 echo "Configuration file updated successfully."
 
-# Timings to add
+# Timings to add, the below 720x480 shifts the image slighly to the left.
 timings=(
     "320 1 20 32 45 240 1 2 3 16 0 0 0 60.000000 0 6514560 1"
-    "720 1 29 69 117 480 1 3 6 34 0 0 0 30 1 14670150 1"
+    "720 1 46 69 100 480 1 3 6 34 0 0 0 30 1 14670150 1"
     "720 1 29 69 117 576 1 7 6 38 0 0 0 25 1 14656125 1"
+	"640 1 26 61 104 480 1 3 6 34 0 0 0 30 1 13038390 1"
 )
 
 file="/opt/rgbpi/ui/data/timings.dat"
