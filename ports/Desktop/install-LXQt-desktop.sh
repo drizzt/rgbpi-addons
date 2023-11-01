f5c8da402aa1245895b58649565974b89e4bd920
Kevin <99993735+sd2cv@users.noreply.github.com>
2023-10-31 17:11:27 -0700

    Delete ports/Desktop directory

--- ports/Desktop/install-LXQt-desktop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo timedatectl set-timezone UTC
-sudo timedatectl set-ntp true
-sudo apt-get update
-sudo apt-get install lxqt-core xinit xserver-xorg-input-joystick firefox-esr -y
-reboot
